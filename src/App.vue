a2a43f518bbb992175ab66bee0fbac11ca6290b4
Markus <ModernFuzzBall2@hotmail.com>
2023-12-07 08:45:08 -0500

    Hello World!

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import Patreon from './components/Patreon.vue';
+import SoundCloud from './components/SoundCloud.vue';
+
+
+</script>
+
+<template>
+  <header class="fixed top-0 left-0 w-full">
+    <nav class="flex items-center p-8">
+      <a class="font-bold mr-2" href="https://facebook.com/markus.gudelis" target="_blank">Facebook</a>
+      <a class="font-bold mr-2" href="https://instagram.com/markus.gudelis" target="_blank">Instagram</a>
+      <a class="font-bold mr-2" href="https://soundcloud.com/demarkio" target="_blank">SoundCloud</a>
+      <a class="font-bold mr-2" href="https://patreon.com/demarkio" target="_blank">Patreon</a>
+    </nav>
+  </header>
+  <main>
+    <section class="w-full h-screen flex items-center justify-center">
+      <div class="text-[#1E3050]">
+        <h1 class="text-9xl font-black">DeMarkio</h1>
+        <h2 class="text-3xl font-black">The God of Smoke and Mirrors</h2>
+      </div>
+    </section>
+  </main>
+  <footer>
+
+  </footer>
+</template>
